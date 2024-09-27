e20358f7f214878a70bd1c8df7401f7a571d257f
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-09-27 18:07:55 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,13 @@ use syn::{parse, parse_macro_input, Error, Item, ItemFn, ItemStatic, LitInt};
 pub fn startup(attr: TokenStream, item: TokenStream) -> TokenStream {
     match parse_macro_input!(item as Item) {
         Item::Fn(item_fn) => startup_function_impl(attr, item_fn),
-        Item::Static(_item_static) => unimplemented!("Init time evaluated static's aren't implemented yet"),
-        item => compile_error(item, "The `#[startup]` attribute can only be applied to `fn`s or `statics`s"),
+        Item::Static(_item_static) => {
+            unimplemented!("Init time evaluated static's aren't implemented yet")
+        }
+        item => compile_error(
+            item,
+            "The `#[startup]` attribute can only be applied to `fn`s or `statics`s",
+        ),
     }
 }
 
@@ -16,7 +21,10 @@ pub fn startup(attr: TokenStream, item: TokenStream) -> TokenStream {
 pub fn shutdown(attr: TokenStream, item: TokenStream) -> TokenStream {
     match parse_macro_input!(item as Item) {
         Item::Fn(item_fn) => shutdown_impl(attr, item_fn),
-        item => compile_error(item, "The `#[shutdown]` attribute can only be applied to `fn`s"),
+        item => compile_error(
+            item,
+            "The `#[shutdown]` attribute can only be applied to `fn`s",
+        ),
     }
 }
 
@@ -45,7 +53,9 @@ fn shutdown_impl(attr: TokenStream, item_fn: ItemFn) -> TokenStream {
 }
 
 fn compile_error(tokens: impl ToTokens, message: impl Display) -> TokenStream {
-    Error::new_spanned(tokens, message).to_compile_error().into()
+    Error::new_spanned(tokens, message)
+        .to_compile_error()
+        .into()
 }
 
 const fn decimal_digit_length_of_usize() -> usize {
@@ -66,28 +76,26 @@ fn gen_func(
     section: &str,
     body: proc_macro2::TokenStream,
 ) -> TokenStream {
-    let order = parse::<LitInt>(attr).map_or_else(|_| String::new(), |lit| {
-        let num = lit.base10_parse::<usize>().unwrap();
-        format!(".{:0width$}", num, width = decimal_digit_length_of_usize())
-    });
+    let order = parse::<LitInt>(attr).map_or_else(
+        |_| String::new(),
+        |lit| {
+            let num = lit.base10_parse::<usize>().unwrap();
+            format!(".{:0width$}", num, width = decimal_digit_length_of_usize())
+        },
+    );
 
     quote! {
         #function
 
         const _: () = {
             #[used]
-            #[cfg_attr(
-                any(
-                    target_os = "none",
-                    target_os = "linux",
-                    target_os = "netbsd",
-                    target_os = "android",
-                    target_os = "freebsd",
-                    target_os = "openbsd",
-                    target_os = "dragonfly",
-                ), 
-                link_section = concat!(".init_array.", #section, #order)
-            )]
+            #[cfg_attr(target_vendor = "dragonfly", link_section = concat!(".init_array.", #section, #order))]
+            #[cfg_attr(target_vendor = "openbsd", link_section = concat!(".init_array.", #section, #order))]
+            #[cfg_attr(target_vendor = "freebsd", link_section = concat!(".init_array.", #section, #order))]
+            #[cfg_attr(target_vendor = "netbsd", link_section = concat!(".init_array.", #section, #order))]
+            #[cfg_attr(target_vendor = "android", link_section = concat!(".init_array.", #section, #order))]
+            #[cfg_attr(target_vendor = "linux", link_section = concat!(".init_array.", #section, #order))]
+            #[cfg_attr(target_vendor = "none", link_section = concat!(".init_array.", #section, #order))]
             #[cfg_attr(target_vendor = "apple", link_section = "__DATA,__mod_init_func")]
             #[cfg_attr(target_os = "windows", link_section = concat!(".CRT$XCU.", #section, #order))]
             static _DECL: unsafe extern "C" fn() = {
