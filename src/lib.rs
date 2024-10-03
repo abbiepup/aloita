9cc19d0966106c560782e5ee97ec65a1c0a17b72
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-10-03 05:53:49 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use core::fmt::Display;
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, Error, Item, ItemFn, LitInt};
+use syn::{parse, parse_macro_input, Error, Item, ItemFn, LitInt};
 
 #[proc_macro_attribute]
 pub fn startup(attr: TokenStream, item: TokenStream) -> TokenStream {
     let message = "The `#[startup]` attribute can only be applied to `fn`s";
 
     match parse_macro_input!(item as Item) {
-        Item::Fn(item_fn) => startup_impl(attr, item_fn),
+        Item::Fn(item_fn) => startup_impl(attr, item_fn, "constructor"),
         item => compile_error(item, message),
     }
 }
@@ -18,29 +18,31 @@ pub fn shutdown(attr: TokenStream, item: TokenStream) -> TokenStream {
     let message = "The `#[shutdown]` attribute can only be applied to `fn`s";
 
     match parse_macro_input!(item as Item) {
-        Item::Fn(item_fn) => shutdown_impl(attr, item_fn),
+        Item::Fn(item_fn) => shutdown_impl(attr, item_fn, "destructor"),
         item => compile_error(item, message),
     }
 }
 
-fn startup_impl(attr: TokenStream, item_fn: ItemFn) -> TokenStream {
+fn startup_impl(attr: TokenStream, item_fn: ItemFn, section: &str) -> TokenStream {
     let ident = &item_fn.sig.ident;
-    gen_func(&item_fn, attr, "constructor", quote! { #ident(); })
+
+    let body = quote! {
+        #ident();
+    };
+
+    gen_func(&item_fn, attr, body, section)
 }
 
-fn shutdown_impl(attr: TokenStream, item_fn: ItemFn) -> TokenStream {
+fn shutdown_impl(attr: TokenStream, item_fn: ItemFn, section: &str) -> TokenStream {
     let ident = &item_fn.sig.ident;
 
-    gen_func(
-        &item_fn,
-        attr,
-        "destructor",
-        quote! {
-            extern "C" { fn atexit(function: unsafe extern "C" fn()); }
-            unsafe extern "C" fn _onexit() { #ident(); }
-            atexit(_onexit);
-        },
-    )
+    let body = quote! {
+        extern "C" { fn atexit(function: unsafe extern "C" fn()); }
+        unsafe extern "C" fn onexit() { #ident(); }
+        atexit(onexit);
+    };
+
+    gen_func(&item_fn, attr, body, section)
 }
 
 fn compile_error(tokens: impl ToTokens, message: impl Display) -> TokenStream {
@@ -49,30 +51,18 @@ fn compile_error(tokens: impl ToTokens, message: impl Display) -> TokenStream {
         .into()
 }
 
-const fn decimal_digit_length_of_usize() -> usize {
-    let mut count = 0;
-    let mut value = usize::MAX;
-
-    while value > 0 {
-        value /= 10;
-        count += 1;
-    }
-
-    count
-}
-
 fn gen_func(
     function: &ItemFn,
     attr: TokenStream,
-    section: &str,
     body: proc_macro2::TokenStream,
+    section: &str,
 ) -> TokenStream {
-    let order = syn::parse::<LitInt>(attr).map_or_else(
-        |_| String::new(),
-        |lit| {
-            let num = lit.base10_parse::<usize>().unwrap();
-            format!(".{:0width$}", num, width = decimal_digit_length_of_usize())
-        },
+    let order =
+        parse::<LitInt>(attr).map_or_else(|_| 0, |lit| lit.base10_parse::<usize>().unwrap());
+    let order = format!(
+        ".{:0width$}",
+        order,
+        width = usize::MAX.ilog10() as usize + 1
     );
 
     quote! {
